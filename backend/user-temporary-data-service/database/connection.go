6986cb519f960a478e4b262a64360dc915aaf016
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-01-31 21:30:59 +0700

    backend add .env

--- backend/user-temporary-data-service/database/connection.go
+++ backend/user-temporary-data-service/database/connection.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/pterm/pterm"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
+	"path/filepath"
 	"user-temporary-data-service/models"
 )
 
@@ -16,13 +20,35 @@ func Connect() {
 	при локальной разработке host = localhost
 	при деплое в продакшэн заменить на host = postgres
 	*/
-	dsn := "host=localhost user=postgres password=25101989 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	wd, err := os.Getwd()
+	if err != nil {
+		pterm.Fatal.Printfln("Error getting working directory: %v", err)
+	}
+
+	// Construct the absolute path to the .env file
+	envPath := filepath.Join(wd, ".env")
+
+	// Load the .env file
+	err = godotenv.Load(envPath)
+	if err != nil {
+		pterm.Fatal.Printfln("Error loading .env file: %v", err)
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DATABASE")
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// Log the DSN for debugging purposes
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", dbHost, dbUser, dbPassword, dbName, dbPort)
 
 	//Подключение к базе данных postgres
 	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		pterm.Fatal.Printfln("Cloud not connect to the database")
+		fmt.Println("Cloud not connect to the database")
+		return
 	}
 
 	DB = connection
--- backend/user_auth_controller/go.mod
+++ backend/user_auth_controller/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-resty/resty/v2 v2.11.0
 	github.com/gofiber/fiber/v2 v2.52.0
 	github.com/lib/pq v1.10.9
+	github.com/pterm/pterm v0.12.76
 	golang.org/x/crypto v0.18.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
@@ -18,22 +19,20 @@ require (
 	atomicgo.dev/schedule v0.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/containerd/console v1.0.3 // indirect
-	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/gookit/color v1.5.4 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/pterm/pterm v0.12.76 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
