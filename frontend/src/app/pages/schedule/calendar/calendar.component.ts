818dcfaf1ec5049ffce70c988d518c75ac5c6eb0
Andrew <andrew.nevsky@yandex.ru>
2024-04-14 21:57:54 +0700

    Календарь событий
    delete

--- frontend/src/app/pages/schedule/calendar/calendar.component.ts
+++ frontend/src/app/pages/schedule/calendar/calendar.component.ts
@@ -7,7 +7,6 @@ import { AsyncPipe, DatePipe, NgForOf, NgIf } from '@angular/common'
 import { GoogleCalendarService } from '../schedule-service/service'
 import { events } from '@material/dom'
 import { HttpClientModule } from '@angular/common/http'
-import { environment } from '../../../environment'
 
 @Component({
   selector: 'app-calendar',
@@ -27,18 +26,7 @@ import { environment } from '../../../environment'
     GoogleCalendarService
   ],
   template: `
-    <div *ngIf="events">
-      <h2>Calendar Events</h2>
-      <ul>
-        <li *ngFor="let event of event">
-          <strong>{{ event.summary }}</strong> - {{ event.start.dateTime | date }} - {{ event.end.dateTime | date }}
-        </li>
-      </ul>
-    </div>
-    
-    <div>
-      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FNovosibirsk&bgcolor=%233F51B5&showTitle=0&showPrint=0&showTz=0&src=aG9wcGVycGxheWVyMEBnbWFpbC5jb20&src=cnUucnVzc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%230B8043" style="border:solid 1px #777" width="1024" height="1024" frameborder="0" scrolling="no"></iframe>
-    </div>
+  
   `,
   styles: [`
 
@@ -46,7 +34,7 @@ import { environment } from '../../../environment'
 })
 
 export class CalendarComponent implements OnInit {
-  calendarId= environment.calendarId
+  calendarId= 'primary' //calendarId
   event: any[] = []
 
   constructor(
