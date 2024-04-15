f9840476acd20b3a4c70caf3081dd09416c84d97
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-04-15 21:35:39 +0700

    UserPersonalPagePortalComponent
    upd

--- frontend/src/app/pages/mypersonalpage/user-personal-page-portal/user-personal-page-portal.component.ts
+++ frontend/src/app/pages/mypersonalpage/user-personal-page-portal/user-personal-page-portal.component.ts
@@ -6,16 +6,16 @@ import {
   MatCardContent,
   MatCardHeader,
   MatCardImage,
-  MatCardModule
-} from '@angular/material/card'
+  MatCardModule,
+} from '@angular/material/card';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { EditUserAdminComponent } from '../../admin-page-setting/user-page-tab-admin/edit-user-admin/edit-user-admin.component';
 import { MatDialog } from '@angular/material/dialog';
 import { StudentLogBookComponent } from '../student-log-book/student-log-book.component';
-import { MatButton } from '@angular/material/button'
-import { AuthenticationService } from '../../../services/authentication.service'
-import { HttpClient } from '@angular/common/http'
-import { Emitters } from '../../../emitters/emitters'
+import { MatButton } from '@angular/material/button';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { HttpClient } from '@angular/common/http';
+import { Emitters } from '../../../emitters/emitters';
 
 @Component({
   selector: 'app-user-personal-page-portal',
@@ -32,116 +32,100 @@ import { Emitters } from '../../../emitters/emitters'
     MatCardHeader,
     MatCardActions,
     MatCardImage,
-    MatCardModule
-  ],providers: [
-    StudentLogBookComponent
+    MatCardModule,
   ],
+  providers: [StudentLogBookComponent],
   template: `
-    <div class="user-personal-page">
-
-      <mat-card-content>
-        <div class="user-info">
-          <img class="user-photo" src="/assets/img.jpg" alt="user-photo">
-        </div>
-      </mat-card-content>
-
-      <div class="user-cards">
-        <mat-card>
-          <mat-card-header>
-            <mat-card-title>Информация</mat-card-title>
-          </mat-card-header>
-            <mat-card-content>
-              <p>{{currentUserRole}}</p>
-              <p>ФИО: {{ currentUser }}</p>
-              <p>Факультет: </p>
-              <p>Группа: </p>
-            </mat-card-content>
-        </mat-card>
-        
-        <mat-card>
-          <mat-card-header>
-            <mat-card-title>Успеваемость</mat-card-title>
-          </mat-card-header>
-          <mat-card-content>
-            <p>Средний балл: </p>
-            <p>Количество пройденных курсов: </p>
-          </mat-card-content>
-        </mat-card>
-        
-        <mat-card>
-          <mat-card-header>
-            <mat-card-title>Контактная информация</mat-card-title>
-          </mat-card-header>
-          <mat-card-content>
-            <p>Email: </p>
-            <p>Телефон: </p>
-          </mat-card-content>
-        </mat-card>
-        
-      </div>
-
-      <div class="user-actions">
-        <button mat-button>
-          Редактировать профиль
-        </button>
-      </div>
-    </div>
-    <div>
+    <div id="header" style="padding: 16px">
       <button mat-button (click)="openStudentLogBook()">
         Журнал успеваемости
       </button>
+      <button mat-button>Редактировать профиль</button>
     </div>
+    <div id="main" style="padding: 16px">
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>Информация</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <p>{{ currentUserRole }}</p>
+          <p>ФИО: {{ currentUser }}</p>
+          <p>Факультет:</p>
+          <p>Группа:</p>
+        </mat-card-content>
+      </mat-card>
 
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>Успеваемость</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <p>Средний балл:</p>
+          <p>Количество пройденных курсов:</p>
+        </mat-card-content>
+      </mat-card>
+
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>Контактная информация</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <p>Email: {{ emailUser }}</p>
+          <p>Телефон:</p>
+        </mat-card-content>
+      </mat-card>
+    </div>
   `,
-  styles: [`
-      .user-personal-page {
-          display: flex;
-          flex-direction: row;
+  styles: [
+    `
+      #cssportal-grid {
+        display: grid;
+        grid-template-rows: 80px 1fr 80px;
+        grid-template-columns: 20% 1fr 20%;
+        gap: 10px;
+        width: 100%;
+        height: 100%;
       }
-
-      .user-info {
-          margin-right: 20px;
-          width: 200px;
-          text-align: center;
+      #header {
+        grid-area: 1 / 1 / 2 / 4;
       }
-
-      .user-cards {
-          flex: 1;
+      #main {
+        grid-area: 2 / 1 / 3 / 4;
       }
-
-      .user-actions {
-          margin-top: 20px;
+      #footer {
+        grid-area: 3 / 1 / 4 / 4;
       }
-      .user-photo {
-          width: auto;
-          height: auto;
-      }
-  `],
+    `,
+  ],
 })
 /*
 TODO: @UserPersonalPagePortalComponent  -> компонент для работы с личной информацией о пользователе
 *
 */
 export class UserPersonalPagePortalComponent implements OnInit {
-  currentUser:any;
-  currentUserRole :any;
+  currentUser: any;
+  currentUserRole: any;
+  emailUser: any;
   constructor(
     private _dialog: MatDialog,
     private _authService: AuthenticationService,
     private _http: HttpClient
   ) {}
   ngOnInit() {
-    this._http.get('http://localhost:8000/api/user',{withCredentials:true}).subscribe(
-      (res:any) =>{
-        this.currentUser = `${res.last_name} ${res.first_name} ${res.middle_name}`;
-        this.currentUserRole=`${res.role}`
-        Emitters.authEmitter.emit(true);
-      },
-      err => {
-        this.currentUser = 'You are not logged in';
-        Emitters.authEmitter.emit(false);
-      }
-    )
+    this._http
+      .get('http://localhost:8000/api/user', { withCredentials: true })
+      .subscribe(
+        (res: any) => {
+          this.currentUser = `${res.last_name} ${res.first_name} ${res.middle_name}`;
+          this.currentUserRole = `${res.role}`;
+          this.emailUser = `${res.email}`;
+          Emitters.authEmitter.emit(true);
+        },
+        (err) => {
+          this.currentUser = 'You are not logged in';
+          Emitters.authEmitter.emit(false);
+        }
+      );
   }
 
   // TODO: Открытие модального окна журнала успеваемости студента
@@ -151,8 +135,8 @@ export class UserPersonalPagePortalComponent implements OnInit {
       Тут реализуется логика получения данных из журнала успеваемости конкретного студента
       под которым авторизовался пользователь
       */
-     width: '1000px',
-     height: '600px',
+      width: '1000px',
+      height: '600px',
     });
   }
 }
